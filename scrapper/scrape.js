4a72629e91f3da6cae2fa33f0fe5522fa50038c3
Santhosh <santhoshm12052003@gmail.com>
2022-01-03 22:54:57 +0530

    .env.example updated

--- scrapper/scrape.js
+++ scrapper/scrape.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer')
 
-const blogModel = require('./database/models/blogs')
+const blogModel = require('../database/models/blogs')
 class scrape {
     constructor(url) {
         this.url = url
